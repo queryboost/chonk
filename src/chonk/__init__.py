e7079c38b5e7df4629446d351887c230bf4cb9a3
JohnsonKuan <johnson.h.kuan@gmail.com>
2026-04-07 13:03:52 -0700

    Update

--- src/chonk/__init__.py
+++
@@ -1,3 +0,0 @@
-"""chonk - Turn your documents into an analytics-ready wide table."""
-
-__version__ = "0.0.1"
---
+++ src/chonkster/__init__.py
@@ -0,0 +1,3 @@
+"""chonkster - Turn your documents into an analytics-ready wide table."""
+
+__version__ = "0.0.1"
